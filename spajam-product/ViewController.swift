0c06aa9855ffd48f399c5650648c232d9aa768fd
choccho <shocho0101@hotmail.co.jp>
2020-10-09 12:23:02 +0900

    Xibを使うように変更

--- spajam-product/ViewController.swift
+++ spajam-product/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: Bundle(for: type(of: self)))
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
