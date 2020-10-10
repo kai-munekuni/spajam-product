d7115098f2d9deea13e590ff2581021aa66bf75e
Hiroya Nakajima <hiroya.nakajima@gmail.com>
2020-10-10 21:58:28 +0900

    widgetのガワ追加 (#4)
    レビューありがぽね

--- Widgets/Widgets.swift
+++ Widgets/Widgets.swift
@@ -10,60 +10,101 @@ import SwiftUI
 import Intents
 
 struct Provider: IntentTimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+    func placeholder(in context: Context) -> WidgetInfo {
+        WidgetInfo(date: Date(), nutrient: "ビタミンC", vegetable: "トマト", image: Image("leaf"), configuration: ConfigurationIntent())
     }
-
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+    
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (WidgetInfo) -> ()) {
+        let entry = WidgetInfo(date: Date(), nutrient: "ビタミンC", vegetable: "トマト", image: Image("leaf"), configuration: configuration)
         completion(entry)
     }
-
+    
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
+        var entries: [WidgetInfo] = []
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            let entry = WidgetInfo(date: entryDate, nutrient: "ビタミンC", vegetable: "トマト", image: Image("leaf"), configuration: configuration)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
 }
 
-struct SimpleEntry: TimelineEntry {
-    let date: Date
+struct WidgetInfo: TimelineEntry {
+    var date: Date
+    let nutrient: String
+    let vegetable: String
+    let image: Image
     let configuration: ConfigurationIntent
 }
 
 struct WidgetsEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
-        Text(entry.date, style: .time)
+        GeometryReader { geometry in
+            HStack(spacing: 0){
+                VStack(spacing: 16){
+                    VStack(alignment: .leading, spacing: 4) {
+                        HStack(spacing: 5) {
+                            Image("leaf").resizable()
+                                .frame(width: 17, height: 17, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            Text("不足中の栄養素")
+                                .foregroundColor(Color("Primary"))
+                        }
+                        Text(entry.nutrient + "不足中")
+                    }
+                    VStack(alignment: .leading, spacing: 4) {
+                        HStack(spacing: 5) {
+                            Image("leaf")
+                                .resizable()
+                                .frame(width: 17, height: 17, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            Text("おすすめの野菜")
+                                .foregroundColor(Color("Primary"))
+                        }
+                        Text(entry.nutrient)
+                    }
+                }
+                .frame(width: geometry.size.width / 2, height: geometry.size.height, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                VStack{
+                    HStack {
+                        entry.image
+                            .resizable()
+                            .frame(width: 100, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        Image("leaf")
+                            .resizable()
+                            .frame(width: 20, height: 20, alignment: .center)
+                    }
+                }
+                .frame(width: geometry.size.width / 2, height: geometry.size.height, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+        }
+        
     }
 }
 
 @main
 struct Widgets: Widget {
     let kind: String = "Widgets"
-
+    
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             WidgetsEntryView(entry: entry)
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
+        .supportedFamilies([.systemMedium])
     }
 }
 
 struct Widgets_Previews: PreviewProvider {
     static var previews: some View {
-        WidgetsEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+        WidgetsEntryView(entry: WidgetInfo(date: Date(), nutrient: "ビタミンC", vegetable: "トマト", image: Image("leaf"), configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
     }
 }
