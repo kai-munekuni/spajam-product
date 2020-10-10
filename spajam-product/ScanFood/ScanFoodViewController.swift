5aeced6b2f0673fc8e45f763beb0425917739ef0
fummicc1 <fumiya.tennis1@gmail.com>
2020-10-10 22:37:28 +0900

    UIだけ作った

--- spajam-product/ScanFood/ScanFoodViewController.swift
+++ spajam-product/ScanFood/ScanFoodViewController.swift
@@ -13,19 +13,80 @@ import UIKit
 
 final class ScanFoodViewController: ViewController {
     
+    enum Const {
+        static let heightDiff: CGFloat = 70
+        static let topMarginAfterScanning: CGFloat = -20
+    }
+    
     @IBOutlet private weak var stackView: UIStackView!
-    @IBOutlet private weak var scanButton: UIButton!
+    @IBOutlet private weak var scanButtonContainer: ScanButtonContainer!
     @IBOutlet private weak var captureView: UIView!
+    @IBOutlet private weak var nutritionCotainerViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var parentStackViewTopConstriant: NSLayoutConstraint!
+    @IBOutlet private weak var nutritionContaienrView: NutritionListContainerView!
     
     private let session: AVCaptureSession = AVCaptureSession()
     
+    private var maskLayer: CAShapeLayer?
     private var cameraPreviewLayer : AVCaptureVideoPreviewLayer?
     private var photoOutput : AVCapturePhotoOutput?
     private var currentDevice: AVCaptureDevice?
     
+    private let viewModel: ScanFoodViewModel = ScanFoodViewModel()
+    private let disposeBag: DisposeBag = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configurePreviewLayer()
+        bindViewModel()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        session.stopRunning()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if let maskLayer = maskLayer {
+            setupMaskLayer(mask: maskLayer)
+            return
+        }
+        setupMaskLayer(mask: CAShapeLayer())
+    }
+    
+    private func bindViewModel() {
+        //MARK: Input
+        scanButtonContainer.scanButton.rx.tap.subscribe(onNext: { [weak self] in
+            self?.viewModel.didTapScanButton()
+        })
+        .disposed(by: disposeBag)
+        
+        // MARK: Output
+        Observable.combineLatest(viewModel.scanning, viewModel.nutritions)
+            .filter { $0.0 == false }
+            .subscribe(onNext: { [weak self] (scanning, nutrition) in
+                if scanning {
+                    self?.hideNtritionContainerView()
+                } else {
+                    self?.showNutritionContaienrView(nutritions: nutrition ?? [])
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func showNutritionContaienrView(nutritions: [Any]) {
+        nutritionContaienrView.refreshData(data: nutritions)
+        let baseHeight = scanButtonContainer.frame.height
+        UIView.animate(withDuration: 0.3) { [weak self] in
+            self?.nutritionCotainerViewHeightConstraint.constant = baseHeight + Const.heightDiff
+            self?.parentStackViewTopConstriant.constant = Const.topMarginAfterScanning
+            self?.view.layoutIfNeeded()
+        }
+    }
+    
+    private func hideNtritionContainerView() {
+        
     }
     
     private func configurePreviewLayer() {
@@ -38,6 +99,16 @@ final class ScanFoodViewController: ViewController {
         }
         setupInputOutput()
         setupPreviewLayer()
+        
+        session.startRunning()
+    }
+    
+    private func setupMaskLayer(mask: CAShapeLayer) {
+        let rect = CGRect(x: 0, y: 0, width: captureView.bounds.width, height: captureView.bounds.width)
+        let path = UIBezierPath(rect: rect)
+        mask.fillRule = CAShapeLayerFillRule.evenOdd
+        mask.path = path.cgPath
+        cameraPreviewLayer?.mask = mask
     }
     
     private func setupInputOutput() {
--- spajam-product/ScanFood/ScanFoodViewModel.swift
+++ spajam-product/ScanFood/ScanFoodViewModel.swift
@@ -6,8 +6,17 @@
 //
 
 import Foundation
+import RxCocoa
 import RxSwift
 import Action
 
 final class ScanFoodViewModel {
+    
+    let scanning: BehaviorRelay<Bool> = .init(value: true)
+    let nutritions: BehaviorRelay<[Any]?> = .init(value: nil)
+    
+    func didTapScanButton() {
+        scanning.accept(false)
+        nutritions.accept(["Test"])
+    }
 }
