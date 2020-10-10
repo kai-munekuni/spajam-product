2c1f0871e1c8a8cf31abc396c27555fc7a1f92bb
fummicc1 <fumiya.tennis1@gmail.com>
2020-10-10 17:35:28 +0900

    wip

---
+++ spajam-product/ScanFood/ScanFoodViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ScanFoodViewController.swift
+//  spajam-product
+//
+//  Created by Fumiya Tanaka on 2020/10/10.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import UIKit
+
+final class ScanFoodViewController: ViewController {
+    
+    @IBOutlet private weak var stackView: UIStackView!
+    @IBOutlet private weak var scanButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ spajam-product/ScanFood/ScanFoodViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ScanFoodViewModel.swift
+//  spajam-product
+//
+//  Created by Fumiya Tanaka on 2020/10/10.
+//
+
+import Foundation
+import RxSwift
+import Action
+
+final class ScanFoodViewModel {
+}
